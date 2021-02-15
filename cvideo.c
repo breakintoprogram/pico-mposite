268f82f8e53854dd31974a7defcab2a2fe068875
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2021-02-15 19:26:54 +0000

    Refactored to use less RAM

--- cvideo.c
+++ cvideo.c
@@ -2,9 +2,11 @@
 // Title:	        Pico-mposite Video Output
 // Author:	        Dean Belfield
 // Created:	        26/01/2021
-// Last Updated:	26/01/2021
+// Last Updated:	15/02/2021
 //
 // Modinfo:
+// 15/02/2021:      Border buffers now have horizontal sync pulse set correctly
+//                  Decreased RAM usage by updating the image buffer scanline on the fly during the horizontal interrupt
 // 
 
 #include "memory.h"
@@ -39,7 +41,7 @@ unsigned char vsync_ll[hdots+1];					// buffer for a vsync line with a long/long
 unsigned char vsync_ls[hdots+1];					// buffer for a vsync line with a long/short pulse
 unsigned char vsync_ss[hdots+1];					// Buffer for a vsync line with a short/short pulse
 unsigned char border[hdots+1];						// Buffer for a vsync line for the top and bottom borders
-unsigned char pixel_buffer[height][hdots+1];		// Buffer for all the vsync lines for the image
+unsigned char pixel_buffer[2][hdots+1];	        	// Double-buffer for the pixel data scanlines
 
 int main() {
     PIO pio = pio0;
@@ -56,35 +58,41 @@ int main() {
     write_vsync_s(&vsync_ss[0],        hdots>>1);			// A short/short vscan line
     write_vsync_s(&vsync_ss[hdots>>1], hdots>>1);
 
-    memset(&border[0], border_colour, hdots);				// And fill the border with the border colour
+    // This bit pre-builds the border scanline
+    //
+    memset(&border[0], border_colour, hdots);				// Fill the border with the border colour
+    memset(&border[0], 1, hsync_bp1);				        // Add the hsync pulse
+    memset(&border[hsync_bp1], 9, hsync_bp2);
 
-	// This bit pre-builds the bitmap by adding the horizontal border stripes and the hsync pulse
+	// This bit pre-builds the pixel buffer scanlines by adding the hsync pulse and left and right horizontal borders 
 	//
-    for(int i = 0; i < height; i++) {								// Loop through the pixel buffer
-        memset(&pixel_buffer[i][0], border_colour, hdots);			// First fill the buffer with the border colour
-        memset(&pixel_buffer[i][0], 1, hsync_bp1);					// Add the hsync pulse
+    for(int i = 0; i < 2; i++) {							// Loop through the pixel buffers
+        memset(&pixel_buffer[i][0], border_colour, hdots);	// First fill the buffer with the border colour
+        memset(&pixel_buffer[i][0], 1, hsync_bp1);			// Add the hsync pulse
         memset(&pixel_buffer[i][hsync_bp1], 9, hsync_bp2);
         memset(&pixel_buffer[i][pixel_start], 31, width);
-        memcpy(&pixel_buffer[i][pixel_start], &bitmap[i], width);	// Copy the bitmap into the middle
     }
 
 	// Initialise the PIO
 	//
-    pio_sm_set_enabled(pio, state_machine, false);
-    pio_sm_clear_fifos(pio, state_machine);	
-    cvideo_initialise_pio(pio, state_machine, offset, 0, 5, piofreq);
-    cvideo_configure_pio_dma(pio, state_machine, dma_channel, pixel_buffer[0], hdots+1);
-    pio_sm_set_enabled(pio, state_machine, true);
+    pio_sm_set_enabled(pio, state_machine, false);                      // Disable the PIO state machine
+    pio_sm_clear_fifos(pio, state_machine);	                            // Clear the PIO FIFO buffers
+    cvideo_initialise_pio(pio, state_machine, offset, 0, 5, piofreq);   // Initialise the PIO (function in cvideo.pio)
+    cvideo_configure_pio_dma(pio, state_machine, dma_channel, hdots+1); // Hook up the DMA channel to the state machine
+    pio_sm_set_enabled(pio, state_machine, true);                       // Enable the PIO state machine
 
-	// And the DMA
+	// And kick everything off
 	//
-    cvideo_dma_handler();
-    while (true) {
+    cvideo_dma_handler();       // Call the DMA handler as a one-off to initialise it
+    while (true) {              // And then just loop doing nothing
         tight_loop_contents();
     }
 }
 
 // Write out a short vsync pulse
+// Parameters:
+// - p: Pointer to the buffer to store this sync data
+// - length: The buffer size
 //
 void write_vsync_s(unsigned char *p, int length) {
     int pulse_width = length / 16;
@@ -94,6 +102,9 @@ void write_vsync_s(unsigned char *p, int length) {
 }
 
 // Write out a long vsync half-pulse
+// Parameters:
+// - p: Pointer to the buffer to store this sync data
+// - length: The buffer size
 //
 void write_vsync_l(unsigned char *p, int length) {
     int pulse_width = length - (length / 16) - 1;
@@ -103,42 +114,56 @@ void write_vsync_l(unsigned char *p, int length) {
 }
 
 // The hblank interrupt handler
+// This is triggered by the instruction irq set 0 in the PIO code (cvideo.pio)
 // 
 void cvideo_dma_handler(void) {
-    void * p_data;
-    dma_hw->ints0 = 1u << dma_channel;		// Clear the interrupt request
-    switch(vline) {							// What scanline are we on?
-        case 1 ... 2:
-            p_data = vsync_ll;   			// Do the vertical sync signals first
+
+    // Switch condition on the vertical scanline number (vline)
+    // Each statement does a dma_channel_set_read_addr to point the PIO to the next data to output
+    //
+    switch(vline) {
+
+        // First deal with the vertical sync scanlines
+        // Also on scanline 3, preload the first pixel buffer scanline
+        //
+        case 1 ... 2: 
+            dma_channel_set_read_addr(dma_channel, vsync_ll, true);
+            memcpy(&pixel_buffer[bline & 1][pixel_start], &bitmap[bline], width);
             break;
         case 3:
-            p_data = vsync_ls;
+            dma_channel_set_read_addr(dma_channel, vsync_ls, true);
             break;
         case 4 ... 5:
-            p_data = vsync_ss;
-            break;
-        case 6 ... 68:
-            p_data = border;				// Our fake top and bottom borders on these scanline
-            break;
-        case 260 ... 309:
-            p_data = border;
-            break;
         case 310 ... 312:
-            p_data = vsync_ss;				// More vertical sync at the bottom
+            dma_channel_set_read_addr(dma_channel, vsync_ss, true);
             break;
+
+        // Then the border scanlines
+        //
+        case 6 ... 68:
+        case 260 ... 309:
+            dma_channel_set_read_addr(dma_channel, border, true);
+            break;
+
+        // Now point the dma at the first buffer for the pixel data,
+        // and preload the data for the next scanline
+        // 
         default:
-            p_data = pixel_buffer[bline++];	// Otherwise use data in the pixel buffer
+            dma_channel_set_read_addr(dma_channel, pixel_buffer[bline++ & 1], true);    // Set the DMA to read from one of the pixel_buffers
+            memcpy(&pixel_buffer[bline & 1][pixel_start], &bitmap[bline], width);       // And memcpy the next scanline into the other pixel buffer
             break;
     }
 
-	// Reset the DMA buffer and retrigger the DMA
-	//
-    dma_channel_set_read_addr(dma_channel, p_data, true);	
-    vline++;
-    if(vline >= 313) {	// If we've gone past the bottom scanline then
-        vline = 1;		// Reset the scanline counter
-        bline = 0;		// And the pixel buffer row index counter
+    // Increment and wrap the counters
+    //
+    if(vline++ >= 312) {    // If we've gone past the bottom scanline then
+        vline = 1;		    // Reset the scanline counter
+        bline = 0;		    // And the pixel buffer row index counter
     }
+
+    // Finally, clear the interrupt request ready for the next horizontal sync interrupt
+    //
+    dma_hw->ints0 = 1u << dma_channel;		
 }
 
 // Configure the PIO DMA
@@ -146,10 +171,9 @@ void cvideo_dma_handler(void) {
 // - pio: The PIO to attach this to
 // - sm: The state machine number
 // - dma_channel: The DMA channel
-// - buffer: No longer used
 // - buffer_size_words: Number of bytes to transfer
 //
-void cvideo_configure_pio_dma(PIO pio, uint sm, uint dma_channel, unsigned char *buffer, size_t buffer_size_words) {
+void cvideo_configure_pio_dma(PIO pio, uint sm, uint dma_channel, size_t buffer_size_words) {
     pio_sm_clear_fifos(pio, sm);
     dma_channel_config c = dma_channel_get_default_config(dma_channel);
     channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
--- cvideo.h
+++ cvideo.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void cvideo_configure_pio_dma(PIO pio, uint sm, uint dma_channel, unsigned char *buffer, size_t buffer_size_words);
+void cvideo_configure_pio_dma(PIO pio, uint sm, uint dma_channel, size_t buffer_size_words);
 void cvideo_dma_handler(void);
 
 void write_vsync_s(unsigned char *p, int length);
