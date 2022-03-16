a37e5aca00ee3c3506ac60ac717476805659d7af
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-03-16 22:25:40 +0000

    Added extra resolutions and tweaked hsync tables

--- cvideo.h
+++ cvideo.h
@@ -2,20 +2,24 @@
 // Title:	        Pico-mposite Video Output
 // Author:	        Dean Belfield
 // Created:	        26/01/2021
-// Last Updated:	05/02/2022
+// Last Updated:	01/03/2022
 //
 // Modinfo:
 // 31/01/2022:      Tweaks to reflect code changes
 // 02/02/2022:      Added initialise_cvideo
 // 04/02/2022:      Added set_border
 // 05/02/2022:      Added support for colour composite board
+// 20/02/2022:      Bitmap is now dynamically allocated
+// 01/03/2022:      Tweaked sync parameters for colour version
 
 #pragma once
 
-#define opt_colour  0           // Set to 1 for colour version
+#include "config.h"
 
 #define piofreq_0 5.25f         // Clock frequence of state machine for PIO handling sync
-#define piofreq_1  7.0f         // Clock frequency of state machine for PIO handling pixel data
+#define piofreq_1_256 7.00f     // Clock frequency of state machine for PIO handling pixel data at various resolutions
+#define piofreq_1_320 5.60f
+#define piofreq_1_640 2.80f
 
 #define sm_sync 0               // State machine number in the PIO for the sync data
 #define sm_data 1               // State machine number in the PIO for the pixel data   
@@ -33,16 +37,22 @@
 #else
     #define colour_base 0x00
     #define colour_max  0xFF
-    #define HSLO        0x0000
-    #define HSHI        0x0200
-    #define VSLO        0x0000
-    #define VSHI        0x0100
+    #define HSLO        0x4200
+    #define HSHI        0x4000
+    #define VSLO        0x4100
+    #define VSHI        0x4000
     #define BORD        0x8000
     #define gpio_base   0
     #define gpio_count  10
 #endif
 
+unsigned char * bitmap;
+
+int width;
+int height;
+
 int initialise_cvideo(void);
+int set_mode(int mode);
 
 void cvideo_configure_pio_dma(PIO pio, uint sm, uint dma_channel, uint transfer_size, size_t buffer_size,  irq_handler_t handler);
 
