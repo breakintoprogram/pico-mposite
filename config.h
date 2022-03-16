9cda3d354181a238c215a46220fd60773980f42a
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-03-16 22:22:41 +0000

    Add files via upload

---
+++ config.h
@@ -0,0 +1,13 @@
+//
+// Title:	        Pico-mposite Defines
+// Author:	        Dean Belfield
+// Created:	        01/03/2022
+// Last Updated:	01/03/2022
+//
+// Modinfo:
+
+#pragma once
+
+#define version         "1.2"
+#define opt_colour      0       // Set to 0 for monochrome board, 1 for colour board
+#define opt_terminal    0       // Set to 1 to just run the terminal software after boot screen
