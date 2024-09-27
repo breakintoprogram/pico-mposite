8b9f2998ef2dbf94b42c980072f49782639c1819
breakintoprogram <dean.belfield@gmail.com>
2024-09-27 11:25:40 +0100

    Version 1.3

--- config.h
+++ config.h
@@ -2,12 +2,13 @@
 // Title:	        Pico-mposite Defines
 // Author:	        Dean Belfield
 // Created:	        01/03/2022
-// Last Updated:	01/03/2022
+// Last Updated:	27/09/2024
 //
 // Modinfo:
+// 27//09/2024:		Version 1.3
 
 #pragma once
 
-#define version         "1.2"
+#define version         "1.3"
 #define opt_colour      0       // Set to 0 for monochrome board, 1 for colour board
 #define opt_terminal    0       // Set to 1 to just run the terminal software after boot screen
